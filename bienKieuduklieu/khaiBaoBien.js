ee46d7124e8a86f27238ecb4c2db37daae582cb1
Your Name <you@example.com>
2022-10-05 20:37:28 +0700

    khai báo bieenss

---
+++ bienKieuduklieu/khaiBaoBien.js
@@ -0,0 +1,12 @@
+let i=10;
+let f=20.5;
+let b=true;
+let s="Hà Nội;
+
+document.wrirte('i =' + i )
+document.wrirte('<br/>' )
+document.wrirte('f =' + f )
+document.wrirte('<br/>' )
+document.wrirte('b =' + b )
+document.wrirte('<br/>' )
+document.wrirte('s =' + s )
