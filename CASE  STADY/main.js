b7bbd9b545b72960947208d42ad78cfc0b144b36
Your Name <you@example.com>
2022-10-26 13:57:34 +0700

    sdasd

--- CASE  STADY/main.js
+++ CASE  STADY/main.js
@@ -146,3 +146,123 @@ function addProduct() {
 function cancelAdd() {
     document.getElementById("add-product").innerHTML = "";
 }
+
+
+function register() {
+    let dataRegister = ""
+    dataRegister += `
+             <div class="modal">
+    <div class="modal__overlay"></div>
+    <div class="modal__body">
+            <div class="auth-form">
+                <div class="auth-form__container">
+                    <div class="auth-form__header">
+                        <h3 class="auth-form__heading">Đăng ký</h3>
+                        <span class="auth-form__switch-btn">Đăng nhập</span>
+                    </div>
+                    <div class="auth-form__form">
+                        <div class="auth-form__group">
+                            <input type="text" class="auth-form__input" placeholder="Nhập email của bạn">
+                        </div>
+                        <div class="auth-form__group">
+                            <input type="password" class="auth-form__input" placeholder="Nhập password của bạn">
+                        </div>
+                        <div class="auth-form__group">
+                            <input type="password" class="auth-form__input" placeholder="Nhập lại password của bạn">
+                        </div>
+                    </div>
+                    <div class="auth-form__aside">
+                        <p class="auth-form__policy-text">
+                            Bằng việc đăng ký, bạn đã đồng ý với MMP Apple Store về
+                            <a href="" class="auth-form__text-link">Điều khoản dịch vụ</a>&
+                            <a href="" class="auth-form__text-link">Chính sách bảo mật</a>
+                        </p>
+                    </div>
+                    <div class="auth-form__controls">
+                        <button class="btn auth-form__controls-back">TRỞ LẠI</button>
+                        <button class="btn btn--primary">ĐĂNG KÝ</button>
+                    </div>
+                </div>
+
+                <div class="auth-form__socials">
+                    <a href="" class="auth-form__socials--facebook btn btn--size-s btn--with-icon">
+                        <i class="auth-form__socials-icon fa-brands fa-square-facebook"></i>
+                       <span class="auth-form__socials-title">
+                           Kết nối với Facebook
+                       </span>
+                    </a>
+                    <a href="" class="auth-form__socials--google btn btn--size-s btn--with-icon">
+                        <i class="auth-form__socials-icon  fa-brands fa-google-plus"></i>
+                        <span class="auth-form__socials-title">
+                           Kết nối với Google
+                       </span>
+
+                    </a>
+                </div>
+
+            </div>
+
+
+    `
+    document.getElementById("register-add").innerHTML = dataRegister;
+}
+
+
+function longin() {
+    let dataLongin = "";
+    dataLongin += `
+             <div class="modal">
+    <div class="modal__overlay"></div>
+    <div class="modal__body">
+            <div class="auth-form">
+                <div class="auth-form__container">
+                    <div class="auth-form__header">
+                        <h3 class="auth-form__heading">Đăng ký</h3>
+                        <span class="auth-form__switch-btn">Đăng nhập</span>
+                    </div>
+                    <div class="auth-form__form">
+                        <div class="auth-form__group">
+                            <input type="text" class="auth-form__input" placeholder="Nhập email của bạn">
+                        </div>
+                        <div class="auth-form__group">
+                            <input type="password" class="auth-form__input" placeholder="Nhập password của bạn">
+                        </div>
+                        <div class="auth-form__group">
+                            <input type="password" class="auth-form__input" placeholder="Nhập lại password của bạn">
+                        </div>
+                    </div>
+                    <div class="auth-form__aside">
+                        <p class="auth-form__policy-text">
+                            Bằng việc đăng ký, bạn đã đồng ý với MMP Apple Store về
+                            <a href="" class="auth-form__text-link">Điều khoản dịch vụ</a>&
+                            <a href="" class="auth-form__text-link">Chính sách bảo mật</a>
+                        </p>
+                    </div>
+                    <div class="auth-form__controls">
+                        <button class="btn auth-form__controls-back">TRỞ LẠI</button>
+                        <button class="btn btn--primary">ĐĂNG KÝ</button>
+                    </div>
+                </div>
+
+                <div class="auth-form__socials">
+                    <a href="" class="auth-form__socials--facebook btn btn--size-s btn--with-icon">
+                        <i class="auth-form__socials-icon fa-brands fa-square-facebook"></i>
+                       <span class="auth-form__socials-title">
+                           Kết nối với Facebook
+                       </span>
+                    </a>
+                    <a href="" class="auth-form__socials--google btn btn--size-s btn--with-icon">
+                        <i class="auth-form__socials-icon  fa-brands fa-google-plus"></i>
+                        <span class="auth-form__socials-title">
+                           Kết nối với Google
+                       </span>
+
+                    </a>
+                </div>
+
+            </div>
+
+
+    `
+    document.getElementById("longin-add").innerHTML = dataLongin;
+}
