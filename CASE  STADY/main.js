1e72cb244938a28fc482d6fdd6aaaa718414cb0c
Your Name <you@example.com>
2022-10-26 15:34:29 +0700

    dasdas

--- CASE  STADY/main.js
+++ CASE  STADY/main.js
@@ -214,55 +214,53 @@ function longin() {
              <div class="modal">
     <div class="modal__overlay"></div>
     <div class="modal__body">
-            <div class="auth-form">
-                <div class="auth-form__container">
-                    <div class="auth-form__header">
-                        <h3 class="auth-form__heading">Đăng ký</h3>
-                        <span class="auth-form__switch-btn">Đăng nhập</span>
+                    <div class="auth-form">
+            <div class="auth-form__container">
+                <div class="auth-form__header">
+                    <h3 class="auth-form__heading">Đăng nhập</h3>
+                    <span class="auth-form__switch-btn">Đăng ký</span>
+                </div>
+                <div class="auth-form__form">
+                    <div class="auth-form__group">
+                        <input type="text" class="auth-form__input" placeholder="Nhập email của bạn">
                     </div>
-                    <div class="auth-form__form">
-                        <div class="auth-form__group">
-                            <input type="text" class="auth-form__input" placeholder="Nhập email của bạn">
-                        </div>
-                        <div class="auth-form__group">
-                            <input type="password" class="auth-form__input" placeholder="Nhập password của bạn">
-                        </div>
-                        <div class="auth-form__group">
-                            <input type="password" class="auth-form__input" placeholder="Nhập lại password của bạn">
-                        </div>
-                    </div>
-                    <div class="auth-form__aside">
-                        <p class="auth-form__policy-text">
-                            Bằng việc đăng ký, bạn đã đồng ý với MMP Apple Store về
-                            <a href="" class="auth-form__text-link">Điều khoản dịch vụ</a>&
-                            <a href="" class="auth-form__text-link">Chính sách bảo mật</a>
-                        </p>
-                    </div>
-                    <div class="auth-form__controls">
-                        <button class="btn auth-form__controls-back">TRỞ LẠI</button>
-                        <button class="btn btn--primary">ĐĂNG KÝ</button>
+                    <div class="auth-form__group">
+                        <input type="password" class="auth-form__input" placeholder="Nhập password của bạn">
                     </div>
                 </div>
-
-                <div class="auth-form__socials">
-                    <a href="" class="auth-form__socials--facebook btn btn--size-s btn--with-icon">
-                        <i class="auth-form__socials-icon fa-brands fa-square-facebook"></i>
-                       <span class="auth-form__socials-title">
-                           Kết nối với Facebook
-                       </span>
-                    </a>
-                    <a href="" class="auth-form__socials--google btn btn--size-s btn--with-icon">
-                        <i class="auth-form__socials-icon  fa-brands fa-google-plus"></i>
-                        <span class="auth-form__socials-title">
-                           Kết nối với Google
-                       </span>
-
-                    </a>
+                <div class="auth-form__aside">
+                   <div class="auth-form__help">
+                       <a href="" class="auth-form__help-link auth-form__help-forgot">Quên mật khẩu</a>
+                       <span class="auth-form__help-separate"></span>
+                       <a href="" class="auth-form__help-link">Cần trợ giúp?</a>
+                   </div>
                 </div>
+                <div class="auth-form__controls">
+                    <button class="btn auth-form__controls-back">TRỞ LẠI</button>
+                    <button class="btn btn--primary">ĐĂNG NHẬP</button>
+                </div>
+            </div>
 
+            <div class="auth-form__socials">
+                <a href="" class="auth-form__socials--facebook btn btn--size-s btn--with-icon">
+                    <i class="auth-form__socials-icon fa-brands fa-square-facebook"></i>
+                    <span class="auth-form__socials-title">
+                           Đăng nhâp với Facebook
+                       </span>
+                </a>
+                <a href="" class="auth-form__socials--google btn btn--size-s btn--with-icon">
+                    <i class="auth-form__socials-icon  fa-brands fa-google-plus"></i>
+                    <span class="auth-form__socials-title">
+                           Đăng nhâp với Google
+                       </span>
+
+                </a>
             </div>
 
 
+            </div>
+   </div>
+            
     `
     document.getElementById("longin-add").innerHTML = dataLongin;
 }
